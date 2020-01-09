1677d2f2318ace66ce4db2fe29fccafaafc1e85d
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-09 18:50:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,4 +6,4 @@ def display_rainbow(colors)
   puts "R: #{rain_colors[0]},O: #{rain_colors[1]},Y: #{rain_colors[2]},G: #{rain_colors[3]},B: #{rain_colors[4]},I: #{rain_colors[5]},V: #{rain_colors[6]}"
 end
 
-display_rainbow
+display_rainbow(rain_colors)
