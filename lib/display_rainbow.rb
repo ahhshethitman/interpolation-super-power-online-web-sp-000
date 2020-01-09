e9f1248d20dc4907ad79cdd706eff65f1cc459ac
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-09 18:51:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 
 rain_colors = ["red","orange","yellow","green","blue","indigo","violet"]
 
-def display_rainbow(colors)
+def display_rainbow(rain_colors)
   puts "R: #{rain_colors[0]},O: #{rain_colors[1]},Y: #{rain_colors[2]},G: #{rain_colors[3]},B: #{rain_colors[4]},I: #{rain_colors[5]},V: #{rain_colors[6]}"
 end
 
